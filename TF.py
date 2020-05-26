2c636787600b460916bede0f47cf306a197b0423
LMD <lisa.dratva@gmail.com>
2020-05-26 15:02:07 +0200

    Initial commit

---
+++ TF.py
@@ -0,0 +1,25 @@
+# names of TFs to inspect in different formats needed for the code
+
+TFs = ['GM12878|BRCA1|None',
+       'GM12801|CTCF|None',
+       'K562|E2F4|None',
+       'GM12878|c-Fos|None',
+       'HeLa-S3|IRF3|None',
+       'H1-hESC|SP4|None',
+       'GM12891|TAF1|None',
+       'K562|STAT2|IFNa6h',
+       'T-47D|ERalpha|Genistein_100nM',
+       'HEK293|KAP1|None']
+
+TFsimple = ['BRCA1', 'CTCF', 'E2F4', 'c-Fos', 'IRF3', 'SP4', 'TAF1', 'STAT2', 'ERalpha', 'KAP1']
+
+TFsSlash = ['GM12878\|BRCA1\|None',
+       'GM12801\|CTCF\|None',
+       'K562\|E2F4\|None',
+       'GM12878\|c-Fos\|None',
+       'HeLa-S3\|IRF3\|None',
+       'H1-hESC\|SP4\|None',
+       'GM12891\|TAF1\|None',
+       'K562\|STAT2\|IFNa6h',
+       'T-47D\|ERalpha\|Genistein_100nM',
+       'HEK293\|KAP1\|None']
